efaf37cc7e867ca56a256e7e5ae5222646efacfa
xbmlz <997909544@qq.com>
2023-06-29 10:57:38 +0800

    first commit

---
+++ venable-framework/src/main/java/com/venable/framework/web/package-info.java
@@ -0,0 +1,1 @@
+package com.venable.framework.web;
---
+++ venable-rest/src/main/java/com/venable/rest/RestServerApplication.java
@@ -0,0 +1,12 @@
+package com.venable.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServerApplication.class, args);
+    }
+}
