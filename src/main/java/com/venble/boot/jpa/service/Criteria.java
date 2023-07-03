c23c6731c5162317d16f904eb6754fd1c34b6ff1
xbmlz <997909544@qq.com>
2023-07-03 15:06:17 +0800

    feat: 增加query service

---
+++ src/main/java/com/venble/boot/jpa/service/Criteria.java
@@ -0,0 +1,13 @@
+package com.venble.boot.jpa.service;
+
+/**
+ * Implementation should usually contain fields of Filter instances.
+ */
+public interface Criteria {
+    /**
+     * <p>copy.</p>
+     *
+     * @return a new criteria with copied filters
+     */
+    Criteria copy();
+}
