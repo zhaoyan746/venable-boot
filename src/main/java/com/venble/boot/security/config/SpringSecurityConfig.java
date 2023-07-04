25c53c4dec8f4e0cc120c37a69e3d3f5d0d7a2fa
xbmlz <997909544@qq.com>
2023-07-04 11:38:09 +0800

    fix: 修复

--- src/main/java/com/venble/boot/security/config/SpringSecurityConfig.java
+++ src/main/java/com/venble/boot/security/config/SpringSecurityConfig.java
@@ -6,7 +6,6 @@ import com.venble.boot.security.filter.JwtAuthenticationFilter;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
